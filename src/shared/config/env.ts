1cc228c0b7cce3c3fe4bbaf85cbbcf66aeab613e
Rafael Domingues <r-domingues@outlook.com>
2023-06-28 22:18:17 -0300

    feat: user auth implementation

---
+++ src/shared/config/env.ts
@@ -0,0 +1,18 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString, validateSync } from 'class-validator';
+
+class Env {
+  @IsString()
+  @IsNotEmpty()
+  jwtSecret: string;
+}
+
+export const env: Env = plainToInstance(Env, {
+  jwtSecret: process.env.JWT_SECRET,
+});
+
+const errors = validateSync(env);
+
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 4));
+}
