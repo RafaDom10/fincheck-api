0e616ebcb09296f21f52af616219356864d6626f
Rafael Domingues <r-domingues@outlook.com>
2023-06-29 19:55:05 -0300

    feat: add categories repository and implement find many

---
+++ src/shared/database/repositories/categories.repositories.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class CategoriesRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  findMany(findManyDto: Prisma.CategoryFindManyArgs) {
+    return this.prismaService.category.findMany(findManyDto);
+  }
+}
