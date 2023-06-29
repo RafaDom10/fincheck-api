4dddbd9b89d0b565de284db8cb9e2c9ef5c9fedd
Rafael Domingues <r-domingues@outlook.com>
2023-06-29 19:05:54 -0300

    refactor: add custom decorator in me method

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/activeUserId';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
-  me(@Req() req: any) {
-    return this.usersService.getUserById(req.userId);
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
