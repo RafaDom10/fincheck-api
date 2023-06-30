ecf11019c56eae1b61b19cacf3cd5d335e9f37c1
Rafael Domingues <r-domingues@outlook.com>
2023-06-30 16:07:20 -0300

    feat: implement bank account service

---
+++ src/modules/bank-accounts/services/bank-accounts.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBankAccountDto } from '../dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from '../dto/update-bank-account.dto';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(
+    private readonly bankAccountsRepository: BankAccountsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+  ) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { name, initialBalance, type, color } = createBankAccountDto;
+    return this.bankAccountsRepository.create({
+      data: {
+        userId,
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountsRepository.findMany({
+      where: { userId },
+    });
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} bankAccount`;
+  }
+
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    const { name, initialBalance, color, type } = updateBankAccountDto;
+
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    return this.bankAccountsRepository.update({
+      where: { id: bankAccountId },
+      data: {
+        name,
+        initialBalance,
+        color,
+        type,
+      },
+    });
+  }
+
+  async remove(userId: string, bankAccountId: string) {
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    await this.bankAccountsRepository.delete({
+      where: { id: bankAccountId },
+    });
+
+    return null;
+  }
+}
