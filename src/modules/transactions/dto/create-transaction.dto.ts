dcfeae3b5e1f20e0479fe9d04d994c10ba2de8f2
Rafael Domingues <r-domingues@outlook.com>
2023-07-02 12:04:39 -0300

    feat: add transaction module

---
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -0,0 +1,39 @@
+import { TransactionType } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+}
